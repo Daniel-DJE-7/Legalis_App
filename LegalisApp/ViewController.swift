79c7b99446d6cbb748857fe8357b94fb25978ae0
memo figueredo <memof80@gmail.com>
2026-04-29 13:54:42 -0500

    Initial Commit

---
+++ LegalisApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LegalisApp
+//
+//  Created by Memo Figueredo on 29/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
