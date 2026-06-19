4e972320a12fb6f3629020ec8f7e0b3a776f7883
memo figueredo <memof80@gmail.com>
2026-06-19 15:47:01 -0500

    I added the file for lawyer's profile and its cell

---
+++ LegalisApp/View/LawyersProfileViewCell/ProfileLawyersCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileLawyersCollectionViewCell.swift
+//  LegalisApp
+//
+//  Created by Daniel Figueredo on 19/6/26.
+//
+
+import UIKit
+
+class ProfileLawyersCollectionViewCell: UICollectionViewCell {
+    
+}
